33d881da1e426f9a4328e8df89cfe7fc277589c1
nieci <nieciowy@gmail.com>
2023-05-11 19:22:43 +0200

    added february and march

--- challenges/views.py
+++ challenges/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works!")
+def january(request):
+    return HttpResponse("Run 3km a day")
+
+
+def february(request):
+    return HttpResponse("Don't eat as much")
+
+
+def march(request):
+    return HttpResponse("Practice guitar an hour per day")
